2bfd1f725af6d0fff457cab832d921210de7af07
Vins13 <13enzo.vp@gmail.com>
2018-12-18 16:17:03 +0100

    aggiunta Student e Secretary

---
+++ src/Model/Student.java
@@ -0,0 +1,89 @@
+package Model;
+
+import Interface.UserInterface;
+
+public class Student implements UserInterface {
+	private String e;
+	private String n;
+	private String su;
+	private char s;
+	private String p;
+	private int u;
+	
+	public Student() {
+	}
+	
+	public Student(String email, String name, String surname, char sex, String password, int userType) {
+		this.e = email;
+		this.n = name;
+		this.su = surname;
+		this.s = sex;
+		this.p = password;
+		this.u = userType;
+	}
+
+	@Override
+public String getEmail() {
+		return e;
+	}
+
+	@Override
+	public String getName() {
+		return n;
+	}
+
+	@Override
+	public String getSurname() {
+		return su;
+	}
+
+	@Override
+	public char getSex() {
+		return s;
+	}
+
+	@Override
+	public String getPassword() {
+		return p;
+	}
+
+	@Override
+	public int getUserType() {
+		return u;
+	}
+
+	@Override
+	public void setEmail(String email) {
+		this.e = email;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.n= name;
+	}
+
+	@Override
+	public void setSurname(String surname) {
+		this.su = surname;
+	}
+
+	@Override
+	public void setSex(char sex) {
+		this.s = sex;
+	}
+
+	@Override
+	public void setPassword(String password) {
+		this.p = password;
+	}
+
+	@Override
+	public void setUserType(int userType) {
+		this.u = userType;
+	}
+
+	@Override
+	public boolean validate() {
+		return false;
+	}
+}
