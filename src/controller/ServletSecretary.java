31748cc883c006b944fae33e4f22aef1ff1d3bfa
GiuseppeCir96 <45792973+GiuseppeCir96@users.noreply.github.com>
2018-12-23 14:50:51 +0100

    Implementazione (parziale) della classe ServletSecretary.java

---
+++ src/controller/ServletSecretary.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class ServletSecretary.
+ */
+@WebServlet("/ServletSecretary")
+public class ServletSecretary extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructor.
+   * @see HttpServlet#HttpServlet()
+   */
+  public ServletSecretary() {
+    super();
+  }
+  
+  /**
+   * Method doGet().
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+  /**
+   * Method doPost().
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+  }
+  
+}
