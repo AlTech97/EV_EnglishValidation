cbf99aa38471533141570de899c646be54ac99d2
GiuseppeCir96 <45792973+GiuseppeCir96@users.noreply.github.com>
2018-12-28 14:12:07 +0100

    Aggiunta classe StateTest.java

--- src/controller/ServletSecretary.java
+++ src/controller/ServletSecretary.java
@@ -77,7 +77,5 @@ public class ServletSecretary extends HttpServlet {
         result = 0;
       }
     } 
-
   }
-
 }
---
+++ src/model/StateTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class StateTest {
+
+  //Test Metodi GET
+
+  @Test
+  void testGetIdState() {
+    State st = new State(00001, "Approved"); 
+    assertEquals(00001, st.getIdState());
+  }
+
+  @Test
+  void testGetDescription() {
+    State st = new State(00001, "Approved");
+    assertEquals("Approved", st.getDescription());
+  }
+
+  //Test Metodi SET
+
+  @Test
+  void testsSetIdState() {
+    State st = new State(00002, "Approved");
+    st.setIdState(00003);
+    assertEquals(00003, st.getIdState());
+  }
+
+  @Test
+  void testSetDescription() {
+    State st = new State(00002, "Denied");
+    st.setDescription("Approved");
+    assertEquals("Approved", st.getDescription()); 
+  }
+}
