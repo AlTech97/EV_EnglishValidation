a15ae6f414b18ef9e747fbb84e3a73e8a5559f95
AngeloMacellaro <angelomacellaro@libero.it>
2018-12-28 11:41:33 +0100

    Rimozione errori di checkstyle presenti nella servlet common

--- src/controller/ServletCommon.java
+++ src/controller/ServletCommon.java
@@ -57,158 +57,144 @@ public class ServletCommon extends HttpServlet {
     String redirect = "";
     PreparedStatement stmt = null;
 
-    int flag = Integer.parseInt(request.getParameter("flag"));   
+    int flag = Integer.parseInt(request.getParameter("flag"));
     Connection conn = new DbConnection().getIstance().getConn();
     String sql = "";
-    
+
     if (conn != null) {
-      
-      if (flag == 1) { // login       
+
+      if (flag == 1) { // login
         String email = request.getParameter("email");
-        String password = new PasswordUtils().generatePwd(request.getParameter("password"));        
-        try {  
-          sql = " SELECT  name, surname, sex, user_type FROM user WHERE TRIM(email) = TRIM(?) AND TRIM(password) = TRIM(?)";
+        String password = new PasswordUtils().generatePwd(request.getParameter("password"));
+        try {
+          sql =
+              " SELECT  name, surname, sex, user_type FROM user "
+              + "WHERE TRIM(email) = TRIM(?) AND TRIM(password) = TRIM(?)";
           stmt = conn.prepareStatement(sql);
           stmt.setString(1, email);
           stmt.setString(2, password);
           ResultSet r = stmt.executeQuery();
           if (r.wasNull()) {
             error = "Errore nell'esecuzione della Query";
-          }
-          else {
+          } else {
             int count = r.last() ? r.getRow() : 0;
             if (count == 1) {
               UserInterface user = null;
               String name = r.getString("name");
               String surname = r.getString("surname");
               char sex = r.getString("sex").charAt(0);
-              
+
               int userType = r.getInt("user_type");
               if (userType == 0) { // Profilo Student
                 redirect = request.getContextPath() + "/_areaStudent/viewRequest.jsp";
                 user = new Student(email, name, surname, sex, password, userType);
-              }
-              else if (userType == 1) { // Profilo Secretary
+              } else if (userType == 1) { // Profilo Secretary
                 redirect = request.getContextPath() + "/_areaSecretary/viewRequest.jsp";
                 user = new Secretary(email, name, surname, sex, password, userType);
-              }
-              else if (userType == 2) { // Profilo Admin
+              } else if (userType == 2) { // Profilo Admin
                 redirect = request.getContextPath() + "/_areaAdmin/viewRequest.jsp";
                 user = new Admin(email, name, surname, sex, password, userType);
               }
-              
+
               request.getSession().setAttribute("user", user);
-              
+
               result = 1;
-            }
-            else {
+            } else {
               error = "Username o Password errati.";
             }
           }
-              
-        
+
+
           if (result == 0) {
             conn.rollback();
           } else {
             conn.commit();
           }
-          
-        }
-        catch (Exception e) {
+
+        } catch (Exception e) {
           error = e.getMessage();
-        }      
-      }
-      else if (flag == 2) { //Aggiornamento Nome
+        }
+      } else if (flag == 2) { // Aggiornamento Nome
         Integer idRequest = Integer.valueOf(request.getParameter("idRequest"));
         String newName = request.getParameter("newName");
-        
-        try {  
+
+        try {
           sql = "SELECT fk_user FROM request WHERE id_request = ?";
           stmt = conn.prepareStatement(sql);
           stmt.setInt(1, idRequest);
           ResultSet r = stmt.executeQuery();
           if (r.wasNull()) {
             error = "Errore nell'esecuzione della Query";
-          }
-          else {            
+          } else {
             int count = r.last() ? r.getRow() : 0;
             if (count == 1) {
               sql = "UPDATE user SET name = ? WHERE email = ?";
               stmt = conn.prepareStatement(sql);
               stmt.setString(1, newName);
-              stmt.setString(1, r.getString("fk_user"));              
+              stmt.setString(1, r.getString("fk_user"));
               if (stmt.executeUpdate() > 0) {
                 content = "Nome Aggiornato";
-                result = 1;                
-              }
-              else {
+                result = 1;
+              } else {
                 error = "Errore aggiornamento Nome.";
               }
-            }
-            else {
+            } else {
               error = "Errore Prelevamento Richiesta";
-            }                                   
+            }
           }
-          
+
           if (result == 0) {
             conn.rollback();
           } else {
             conn.commit();
           }
-        
-        } 
-        catch (Exception e) {
+
+        } catch (Exception e) {
           error = e.getMessage();
-        }                  
-      }
-      else if (flag == 3) { //Aggiornamento Cognome
+        }
+      } else if (flag == 3) { // Aggiornamento Cognome
         Integer idRequest = Integer.valueOf(request.getParameter("idRequest"));
         String newSurname = request.getParameter("newSurname");
-        
-        try {  
+
+        try {
           sql = "SELECT fk_user FROM request WHERE id_request = ?";
           stmt = conn.prepareStatement(sql);
           stmt.setInt(1, idRequest);
           ResultSet r = stmt.executeQuery();
           if (r.wasNull()) {
             error = "Errore nell'esecuzione della Query";
-          }
-          else {            
+          } else {
             int count = r.last() ? r.getRow() : 0;
             if (count == 1) {
               sql = "UPDATE user SET surname = ? WHERE email = ?";
               stmt = conn.prepareStatement(sql);
               stmt.setString(1, newSurname);
-              stmt.setString(1, r.getString("fk_user"));              
+              stmt.setString(1, r.getString("fk_user"));
               if (stmt.executeUpdate() > 0) {
                 content = "Cognome Aggiornato";
-                result = 1;                
-              }
-              else {
+                result = 1;
+              } else {
                 error = "Errore aggiornamento Cognome.";
               }
-            }
-            else {
+            } else {
               error = "Errore Prelevamento Richiesta";
-            }                                   
+            }
           }
-          
+
           if (result == 0) {
             conn.rollback();
           } else {
             conn.commit();
           }
-        
-        }
-        catch (Exception e) {
+
+        } catch (Exception e) {
           error = e.getMessage();
-        }                  
-      }      
-      
-            
-    }
-    else {
-      error = "Nessuna connessione al database.";      
+        }
+      }
+
+
+    } else {
+      error = "Nessuna connessione al database.";
     }
 
 
