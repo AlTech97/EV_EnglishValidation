a0ee1dd562ab3c368edfd789063e443beea6a072
Alfonso <alfonso.ingenito15@gmail.com>
2018-12-28 18:45:39 +0100

    Aggiunta pagina con form richiesta
    Da modificare poichè a me non riconosceva la chiusura di alcuni tag e di conseguenza non funzionava la grafica.
    Completare scripts_firstForm.js pochè non riesco a comprendere dove inserire i controlli sui dati richiesti.

---
+++ WebContent/js/pages/scripts_firstForm.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+	
+	
+	$(document).on('submit', '#signUp', function(e){
+		var year = $("#immatricolazione").val();
+		var graduation = $(".optradio:checked").val();
+		var serial = $("#matricola").val();
+		var ente = $("#ente").val();
+		var expiryDate = $("#datarilascio").val();
+		var certificateSerial = $("#seriale").val();
+		var level = $("#lvlcefr").val();;
+		var requestedCfu = $("#cfu").val();
+
+		if(year != undefined && graduation != undefined && number != undefined && ente != undefined && 
+				expiryDate != undefined && serial != undefined && level != undefined && requestedCfu != undefined){
+				$(".preloader").show();
+
+				$.ajax({
+					url: absolutePath+"/ServletStudent",
+					type: "POST",
+					dataType: 'JSON',
+					async: false,
+					data: {
+						"year": year,
+						"graduation": graduation,
+						"serial": serial,
+						"ente": ente,
+						"expiryDate": expiryDate,
+						"number": number,
+						"level": level,
+						"requestedCfu": requestedCfu,
+						"flag": 2
+					},
+					success:function(msg){
+						if(!msg.risultato){
+							showAlert(1, msg.errore);
+						}
+						else{
+							showAlert(0, msg.contenuto);
+							
+							setTimeout(function (){
+								window.location.href = msg.redirect;
+							},2000);
+						}
+					},
+					error: function(msg){
+						showAlert(1, "Impossibile Recuperare i dati.");
+					}
+				});
+				
+				$(".preloader").hide();				
+			}			
+		else{
+			showAlert(1, "Errore prelevamento campi.");
+		}
+
+		return false;
+	});	
+	
+	
+	
+	
+	
+});
