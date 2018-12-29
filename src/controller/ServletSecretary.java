400d33c8dba6acb1116dab5fb8e557a482b44620
GiuseppeCir96 <45792973+GiuseppeCir96@users.noreply.github.com>
2018-12-29 21:12:50 +0100

    Implemantazione ServletSecretary.java

--- src/controller/ServletSecretary.java
+++ src/controller/ServletSecretary.java
@@ -46,36 +46,89 @@ public class ServletSecretary extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    Integer cfu = Integer.parseInt(request.getParameter("cfu"));
     Integer result = 0;
     String error = "";
     String content = "";
+    PreparedStatement stmt = null;
 
-    new DbConnection();
-    Connection connDb = DbConnection.getInstance().getConn();
-    if (((DbConnection) connDb).getConn() != null) {
-      try {
-        String sql = "INSERT INTO REQUEST(validated_CFU) VALUES (NOW()) ;";
-        PreparedStatement stmt = ((DbConnection) connDb).getConn().prepareStatement(sql);
-        stmt.setInt(1, cfu);
-        if (stmt.executeUpdate() == 1) {
-          content = "CFU inseriti con successo.";
-          result = 1;
-        } else {
-          error = "Errore Inserimento CFU.";
+    int flag = Integer.parseInt(request.getParameter("flag"));
+    Connection connDb = new DbConnection().getInstance().getConn();
+    String sql = "";
+
+    if (connDb != null) {
+
+      if (flag == 1) {
+        try {
+          sql = "SELECT * FROM REQUEST;";
+          stmt = ((DbConnection) connDb).getConn().prepareStatement(sql);
+          if (stmt.executeUpdate() == 1) {
+            content = "Success";
+            result = 1;
+          } else {
+            error = "Error";
+            result = 0;
+          }
+
+          if (result == 0) {
+            ((DbConnection) connDb).getConn().rollback();
+          } else {
+            ((DbConnection) connDb).getConn().commit();
+          }
+          ((DbConnection) connDb).getConn().close();
+        } catch (Exception e) {
+          error = "Errore nell'esecuzione Query.";
           result = 0;
         }
+      }  else if (flag == 2) { 
+          
+        Integer idRequest = Integer.valueOf(request.getParameter("idRequest"));
+        Integer cfu = Integer.valueOf(request.getParameter("cfu"));
+  
+        try {
+          sql = "SELECT validated_CFU FROM REQUEST WHERE id_request = ?";
+          stmt = connDb.prepareStatement(sql);
+          stmt.setInt(1, idRequest);
+          ResultSet r = stmt.executeQuery();
+          if (r.wasNull()) {
+            error = "Errore nell'esecuzione della Query";
+          } else {
+            int count = r.last() ? r.getRow() : 0;
+            if (count == 1) {
+              sql = "UPDATE REQUEST SET validated_CFU = ? WHERE id_request = ?";
+              stmt = connDb.prepareStatement(sql);
+              stmt.setInt(1, cfu);
+              stmt.setString(1, r.getString("validated_cfu"));
+              if (stmt.executeUpdate() > 0) {
+                content = "Success";
+                result = 1;
+              } else {
+                error = "Error";
+              }
+            } else {
+              error = "Error";
+            }
+          }
+          
+          if (result == 0) {
+            connDb.rollback();
+          } else {
+            connDb.commit();
+          }
 
-        if (result == 0) {
-          ((DbConnection) connDb).getConn().rollback();
-        } else {
-          ((DbConnection) connDb).getConn().commit();
-        }
-        ((DbConnection) connDb).getConn().close();
-      } catch (Exception e) {
-        error = "Errore esecuzione Query.";
-        result = 0;
+        } catch (Exception e) {
+          error += e.getMessage();
+        } 
       }
-    } 
+      
+    } else {
+      error = "Nessuna connessione al database.";
+    }
+    
+    JSONObject res = new JSONObject();
+    res.put("result", result);
+    res.put("error", error);
+    res.put("content", content);
+    PrintWriter out = response.getWriter();
+    out.println(res);
   }
 }
