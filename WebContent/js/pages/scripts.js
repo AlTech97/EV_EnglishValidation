30a00517f90f962b8a6fc9d73dcb658d658a2233
Ivan Buccella <i.buccella@studenti.unisa.it>
2018-12-29 16:00:44 +0100

    Aggiunta funzione sendPostData.

--- WebContent/js/pages/scripts.js
+++ WebContent/js/pages/scripts.js
@@ -1,5 +1,21 @@
 var absolutePath = "/EnglishValidation";
 
+//sendPostData(msg.redirect, { 'idRequest': msg.contenuto });
+function sendPostData(url, data) {
+    var form = document.createElement('form');
+    document.body.appendChild(form);
+    form.method = 'post';
+    form.action = url;
+    for (var name in data) {
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = data[name];
+        form.appendChild(input);
+    }
+    form.submit();
+}
+
 function showAlert(flag, descrizione){
 	toastr.options = {
 	  "closeButton": true,
