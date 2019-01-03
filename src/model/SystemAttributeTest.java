abe860b85f69bcb97adc09029f7749f45288e3cf
GiuseppeCir96 <45792973+GiuseppeCir96@users.noreply.github.com>
2019-01-03 16:39:35 +0100

    Completamento classe SystemAttributeTest.java

--- src/model/SystemAttributeTest.java
+++ src/model/SystemAttributeTest.java
@@ -18,20 +18,69 @@ public class SystemAttributeTest {
 
   @Test
   void testGetHashMap() {
-    SystemAttribute sa = new SystemAttribute();
-    HashMap<String, String> hashMap = new HashMap<>();
-    hashMap.put("request-partially-completed", "1");
     HashMap<String, String> expected = new HashMap<>();
+    expected.put("request-accepted", "6");
+    expected.put("request-allowed-extension-upload", ".pdf");
+    expected.put("request-matriculation-year-range", "5");
+    expected.put("request-max-cfu", "12");
+    expected.put("request-min-cfu", "1");
+    expected.put("request-number-max-upload", "2");
     expected.put("request-partially-completed", "1");
+    expected.put("request-refused", "7");
+    expected.put("request-upload-path", "E:\\Progetto-IS\\uploads\\");
+    expected.put("request-working-admin", "3");
+    expected.put("request-working-educational-advice-1", "4");
+    expected.put("request-working-educational-advice-2", "5");
+    expected.put("request-working-secretary", "2");
 
-    assertThat(hashMap, is(expected));
-    assertThat(hashMap.size(), is(1));
-    assertThat(hashMap, IsMapContaining.hasEntry("request-partially-completed", "1"));
-    assertThat(hashMap, not(IsMapContaining.hasEntry("aaa", "a")));
-    assertThat(hashMap, IsMapContaining.hasKey("request-partially-completed"));
-    assertThat(hashMap, IsMapContaining.hasValue("1"));
-    assertEquals("request-partially-completed" + hashMap.get("request-partially-completed"), sa.getHashMap());
+    SystemAttribute sa = new SystemAttribute();
+    assertThat(expected, is(sa.getHashMap()));
+    assertThat(sa.getHashMap().size(), is(13));
+    assertThat(sa.getHashMap(), IsMapContaining.hasEntry("request-accepted", "6"));
+    assertThat(sa.getHashMap(), not(IsMapContaining.hasEntry("aaa", "a")));
+    assertThat(sa.getHashMap(), IsMapContaining.hasKey("request-partially-completed"));
+    assertThat(sa.getHashMap(), IsMapContaining.hasValue("1"));
   }
+  
+  @Test
+  void testSetHshMap() {
+    HashMap<String, String> expected = new HashMap<>();
+    expected.put("request-accepted", "6");
+    expected.put("request-allowed-extension-upload", ".docx");
+    expected.put("request-matriculation-year-range", "5");
+    expected.put("request-max-cfu", "12");
+    expected.put("request-min-cfu", "1");
+    expected.put("request-number-max-upload", "2");
+    expected.put("request-partially-completed", "1");
+    expected.put("request-refused", "7");
+    expected.put("request-upload-path", "E:\\Progetto-IS\\uploads\\");
+    expected.put("request-working-admin", "3");
+    expected.put("request-working-educational-advice-1", "4");
+    expected.put("request-working-educational-advice-2", "5");
+    expected.put("request-working-secretary", "2");
+    SystemAttribute sa = new SystemAttribute();
+    sa.setHashMap(expected);
+    assertThat(expected, is(sa.getHashMap()));
+  }
+  
+  @Test
+  void testGetValueByKey() {
+    SystemAttribute sa = new SystemAttribute();
+    assertEquals("6", sa.getValueByKey("request-accepted"));
+    assertEquals(".pdf", sa.getValueByKey("request-allowed-extension-upload"));
+    assertEquals("5", sa.getValueByKey("request-matriculation-year-range"));
+    assertEquals("12", sa.getValueByKey("request-max-cfu"));
+    assertEquals("1", sa.getValueByKey("request-min-cfu"));
+    assertEquals("2", sa.getValueByKey("request-number-max-upload"));
+    assertEquals("1", sa.getValueByKey("request-partially-completed"));
+    assertEquals("7", sa.getValueByKey("request-refused"));
+    assertEquals("E:\\Progetto-IS\\uploads\\", sa.getValueByKey("request-upload-path"));
+    assertEquals("3", sa.getValueByKey("request-working-admin"));
+    assertEquals("4", sa.getValueByKey("request-working-educational-advice-1"));
+    assertEquals("5", sa.getValueByKey("request-working-educational-advice-2"));
+    assertEquals("2", sa.getValueByKey("request-working-secretary"));
+  }
+
   
   @Test
   void testGetInstance() {
