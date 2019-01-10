160cde200c24b810c030539c2aa87a361c2acfb7
Ivan Buccella <i.buccella@studenti.unisa.it>
2019-01-10 21:38:49 +0100

    FIX VIsualizzazione richieste segreteria.

--- src/controller/ServletSecretary.java
+++ src/controller/ServletSecretary.java
@@ -49,41 +49,6 @@ public class ServletSecretary extends HttpServlet {
    */
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
-    /*
-     * Integer result = 0; String error = ""; String content = ""; PreparedStatement stmt = null;
-     * 
-     * int flag = Integer.parseInt(request.getParameter("flag")); Connection connDb = new
-     * DbConnection().getInstance().getConn(); String sql = "";
-     * 
-     * if (connDb != null) {
-     * 
-     * if (flag == 1) { try { sql = "SELECT * FROM REQUEST;"; stmt = ((DbConnection)
-     * connDb).getConn().prepareStatement(sql); if (stmt.executeUpdate() == 1) { content =
-     * "Success"; result = 1; } else { error = "Error"; result = 0; }
-     * 
-     * if (result == 0) { ((DbConnection) connDb).getConn().rollback(); } else { ((DbConnection)
-     * connDb).getConn().commit(); } ((DbConnection) connDb).getConn().close(); } catch (Exception
-     * e) { error = "Errore nell'esecuzione Query."; result = 0; } } else if (flag == 2) {
-     * 
-     * Integer idRequest = Integer.valueOf(request.getParameter("idRequest")); Integer cfu =
-     * Integer.valueOf(request.getParameter("cfu"));
-     * 
-     * try { sql = "SELECT validated_CFU FROM REQUEST WHERE id_request = ?"; stmt =
-     * connDb.prepareStatement(sql); stmt.setInt(1, idRequest); ResultSet r = stmt.executeQuery();
-     * if (r.wasNull()) { error = "Errore nell'esecuzione della Query"; } else { int count =
-     * r.last() ? r.getRow() : 0; if (count == 1) { sql =
-     * "UPDATE REQUEST SET validated_CFU = ? WHERE id_request = ?"; stmt =
-     * connDb.prepareStatement(sql); stmt.setInt(1, cfu); stmt.setString(1,
-     * r.getString("validated_cfu")); if (stmt.executeUpdate() > 0) { content = "Success"; result =
-     * 1; } else { error = "Error"; } } else { error = "Error"; } }
-     * 
-     * if (result == 0) { connDb.rollback(); } else { connDb.commit(); }
-     * 
-     * } catch (Exception e) { error += e.getMessage(); } }
-     * 
-     * } else { error = "Nessuna connessione al database."; }
-     */
     Integer result = 0; // indica se la query è riuscita
     String error = ""; // indica il mex di errore
     String content = ""; // indica il contenuto trovato dopo la query
@@ -116,6 +81,7 @@ public class ServletSecretary extends HttpServlet {
           if (r.wasNull()) {
             error = "Errore nell'esecuzione della Query";
           } else {
+            result = 1;
             int count = r.last() ? r.getRow() : 0;
             if (count > 0) {
               r.beforeFirst();
@@ -149,9 +115,9 @@ public class ServletSecretary extends HttpServlet {
                     + "\"><i class=\"fa fa-check\"></i></button>";
                 
                 content += "    <td class='text-center'></td>";
-              }
-              
-              result = 1;
+              }              
+            } else {
+              content += "Nessuna Richiesta Presente";
             }
           }
         } catch (Exception e) {
@@ -219,6 +185,7 @@ public class ServletSecretary extends HttpServlet {
 
     }
     else {
+      result = 0;
       error = "Nessuna connesione al DB";
     }
 
