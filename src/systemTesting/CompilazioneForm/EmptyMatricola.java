67db5171cf1073b41d14902fe5dbbea744c6bb49
AngeloMacellaro <angelomacellaro@libero.it>
2019-01-15 21:17:35 +0100

    Rimozione checkstyle nel package systemTesting.CompilazioneForm

--- src/systemTesting/CompilazioneForm/EmptyMatricola.java
+++ src/systemTesting/CompilazioneForm/EmptyMatricola.java
@@ -1,11 +1,15 @@
 package systemTesting.CompilazioneForm;
 
-import java.util.regex.Pattern;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import java.util.regex.Pattern;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -15,6 +19,9 @@ public class EmptyMatricola {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
+  /**
+   * Before.
+   */
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
@@ -32,17 +39,23 @@ public class EmptyMatricola {
     driver.findElement(By.id("password")).click();
     driver.findElement(By.id("password")).clear();
     driver.findElement(By.id("password")).sendKeys("password");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Login'])[3]/following::button[1]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Login'])[3]/following::button[1]"))
+        .click();
     driver.get("http://localhost:8080/EnglishValidation/_areaStudent/firstForm.jsp");
     driver.findElement(By.id("immatricolazione")).click();
     new Select(driver.findElement(By.id("immatricolazione"))).selectByVisibleText("2015/2016");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Richiesta'])[1]/following::option[2]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Richiesta'])[1]/following::option[2]"))
+        .click();
     driver.findElement(By.id("matricola")).click();
     driver.findElement(By.id("matricola")).clear();
     driver.findElement(By.id("matricola")).sendKeys("");
     driver.findElement(By.id("ente")).click();
     new Select(driver.findElement(By.id("ente"))).selectByVisibleText("Pearson - LCCI");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ente di rilascio:'])[1]/following::option[7]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Ente di rilascio:'])[1]/following::option[7]"))
+        .click();
     driver.findElement(By.id("datascadenza")).click();
     driver.findElement(By.id("datascadenza")).clear();
     driver.findElement(By.id("datascadenza")).sendKeys("0002-08-19");
@@ -52,16 +65,25 @@ public class EmptyMatricola {
     driver.findElement(By.id("datascadenza")).sendKeys("0202-08-19");
     driver.findElement(By.id("datascadenza")).clear();
     driver.findElement(By.id("datascadenza")).sendKeys("2020-08-19");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]"))
+        .click();
     driver.findElement(By.id("seriale")).click();
     driver.findElement(By.id("seriale")).clear();
     driver.findElement(By.id("seriale")).sendKeys("123");
     driver.findElement(By.id("lvlcefr")).click();
     new Select(driver.findElement(By.id("lvlcefr"))).selectByVisibleText("A2");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='livello CEFR:'])[1]/following::option[2]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='CFU da Conseguire:'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='livello CEFR:'])[1]/following::option[2]"))
+        .click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='CFU da Conseguire:'])[1]/following::button[1]"))
+        .click();
   }
-
+  
+  /**
+   * After.
+   */
   @After
   public void tearDown() throws Exception {
     driver.quit();
--- src/systemTesting/CompilazioneForm/EmptySeriale.java
+++ src/systemTesting/CompilazioneForm/EmptySeriale.java
@@ -1,11 +1,15 @@
 package systemTesting.CompilazioneForm;
 
-import java.util.regex.Pattern;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import java.util.regex.Pattern;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -15,6 +19,9 @@ public class EmptySeriale {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
+  /**
+   * Before.
+   */
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
@@ -32,19 +39,35 @@ public class EmptySeriale {
     driver.findElement(By.id("password")).click();
     driver.findElement(By.id("password")).clear();
     driver.findElement(By.id("password")).sendKeys("password");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Login'])[3]/following::button[1]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Login'])[3]/following::button[1]"))
+        .click();
     driver.get("http://localhost:8080/EnglishValidation/_areaStudent/firstForm.jsp");
     driver.findElement(By.id("immatricolazione")).click();
     new Select(driver.findElement(By.id("immatricolazione"))).selectByVisibleText("2015/2016");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Richiesta'])[1]/following::option[2]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]")).click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Richiesta'])[1]/following::option[2]"))
+        .click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]"))
+        .click();
     driver.findElement(By.id("ente")).click();
-    new Select(driver.findElement(By.id("ente"))).selectByVisibleText("Educational Testing Service (ETS)");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ente di rilascio:'])[1]/following::option[4]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='CFU da Conseguire:'])[1]/following::button[1]")).click();
+    new Select(driver.findElement(By.id("ente")))
+        .selectByVisibleText("Educational Testing Service (ETS)");
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Ente di rilascio:'])[1]/following::option[4]"))
+        .click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='Disconnetti'])[2]/following::div[11]"))
+        .click();
+    driver.findElement(By.xpath(
+        "(.//*[normalize-space(text()) and normalize-space(.)='CFU da Conseguire:'])[1]/following::button[1]"))
+        .click();
   }
-
+  
+  /**
+   * After.
+   */
   @After
   public void tearDown() throws Exception {
     driver.quit();
